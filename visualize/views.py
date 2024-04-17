c2d76af27ec6aedd81d5a70703a544ba43020590
snowmuffin <tnt0807@naver.com>
2024-04-17 18:42:51 +0900

    상상대로 서울 추가

--- visualize/views.py
+++ visualize/views.py
@@ -24,9 +24,16 @@ def congress_chart(request):
     context = {'posts': view_ordered_posts}
     return render(request, "chart/charts_congress.html", context)
 
+
 def cw24_chart(request):
     view_ordered_posts = CW24.objects.order_by("-views")[:10]
 
     context = {'posts': view_ordered_posts}
     return render(request, "chart/charts_cw24.html", context)
-  
+
+def ideaseoul_chart(request):
+    order_by = request.GET.get("order-by")
+    view_ordered_posts = Ideaseoul.objects.order_by("-rating")[:10]
+
+    context = {'posts': view_ordered_posts}
+    return render(request, "chart/charts_ideaseoul.html", context)
