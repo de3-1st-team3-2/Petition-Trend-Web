234bafab351dd613641d65dc8ad494b0f807507b
7xxogre <bestech82@naver.com>
2024-04-17 22:08:09 +0900

    국민 생각함 모델 추가

--- visualize/models.py
+++ visualize/models.py
@@ -54,3 +54,17 @@ class CW24(models.Model):
 
     def __str__(self):
         return f"제목: {self.title}, 작성일: {self.pub_date}, 추진상황: {self.status}"
+    
+class SubThink(models.Model):
+    title = models.CharField(max_length=50, verbose_name="제목")
+    url = models.CharField(max_length=200, verbose_name="URL")
+    pub_date = models.DateTimeField(verbose_name="작성일")
+    start_date = models.DateTimeField(verbose_name="시작일")
+    end_date = models.DateTimeField(verbose_name="종료일")
+    participants = models.IntegerField(null = True, verbose_name="참여자수")
+    recommends = models.IntegerField(null = True, verbose_name="추천수")
+    no_recommends = models.IntegerField(null = True, verbose_name="비추천수")
+
+    def __str__(self):
+        return f"제목: {self.title}, 작성일: {self.pub_date}, 추진상황: {self.status}"
+    
