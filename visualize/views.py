4c9f1c5b8d159a1361f34f03cb592cd2dd3ce4f7
7xxogre <bestech82@naver.com>
2024-04-17 15:45:41 +0900

    merge를 위한 국민신문고 html 이름 변경

--- visualize/views.py
+++ visualize/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-
+from datetime import datetime
 def main_chart(request):
     order_by = request.GET.get("order-by")
     if order_by == 'rating':
@@ -13,11 +13,12 @@ def main_chart(request):
 
 
 def epeople_chart(request):
+    current_month_start = datetime(datetime.now().year, datetime.now().month, 1)
     order_by = request.GET.get("order-by")
     if order_by == 'rating':
-        view_ordered_posts = Epeople.objects.order_by("-rating")[:10]
+        view_ordered_posts = Epeople.objects.filter(pub_date__gte=current_month_start).order_by("-rating")[:10]
     else:
-        view_ordered_posts = Epeople.objects.order_by("-views")[:10]
+        view_ordered_posts = Epeople.objects.filter(pub_date__gte=current_month_start).order_by("-views")[:10]
 
     context = {'posts': view_ordered_posts}
-    return render(request, "chart/epeople.html", context)
+    return render(request, "chart/charts_epeople.html", context)
